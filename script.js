c2e1c5a42951f0e8cb2fa2b299d8d8c680b9c3af
Willian Negishi <dakarakoso@Willians-MacBook-Pro.local>
2021-09-28 18:39:35 +0900

    started to implement the video player functionality

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-
+const video = document.querySelector("video");
+const progressRange = document.querySelector(".progress-range");
+const progressBar = document.querySelector(".progress-bar");
+const playBtn = document.getElementById("play-btn");
+const volumeIcon = document.getElementById("volume-icon");
+const volumeRange = document.querySelector(".volume-range");
+const volumeBar = document.querySelector(".volume-bar");
+const currentTime = document.querySelector(".time-elapsed");
+const duration = document.querySelector(".item-duration");
+const fullscreenBtn = document.querySelector(".fullscreen");
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("attribute", "Play");
+}
 
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("attribute", "Pause");
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+}
 
+// When video ends show play button instead of paused
+video.addEventListener("ended", showPlayIcon);
 // Progress Bar ---------------------------------- //
-
-
 
 // Volume Controls --------------------------- //
 
-
-
 // Change Playback Speed -------------------- //
-
-
 
 // Fullscreen ------------------------------- //
 
+// Event listener
 
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);

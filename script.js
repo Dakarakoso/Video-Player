1582ea036ec426efeff3814b3da107cf019316c4
Willian Negishi <dakarakoso@Willians-MacBook-Pro.local>
2021-09-29 17:47:02 +0900

    added the functionality to the progressbar

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const volumeIcon = document.getElementById("volume-icon");
 const volumeRange = document.querySelector(".volume-range");
 const volumeBar = document.querySelector(".volume-bar");
 const currentTime = document.querySelector(".time-elapsed");
-const duration = document.querySelector(".item-duration");
+const duration = document.querySelector(".time-duration");
 const fullscreenBtn = document.querySelector(".fullscreen");
 
 // Play & Pause ----------------------------------- //
@@ -29,7 +29,27 @@ function togglePlay() {
 
 // When video ends show play button instead of paused
 video.addEventListener("ended", showPlayIcon);
+
 // Progress Bar ---------------------------------- //
+
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
+
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
+}
+
+function setProgress(e) {
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+}
 
 // Volume Controls --------------------------- //
 
@@ -41,3 +61,6 @@ video.addEventListener("ended", showPlayIcon);
 
 playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
